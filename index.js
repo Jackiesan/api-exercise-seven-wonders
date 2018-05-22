6874b2327a120d48d2be4b6114bbccdf6c4e9dd7
Jackie <jonofre16@hotmail.com>
2018-05-22 13:51:50 -0700

    Completed method to get location of seven wonders

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-
-const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=$Chicago&key=AIzaSyArO73wSW8bJ-WDmqcrVXXNkAh0gFjeP14`;
-
-
 const sevenWonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
+
+const reportStatus = (message) => {
+  $('#status-message').html(message);
+}
 
 const loadCoordinates = () => {
   const coordinateList = $('#coordinate-list')
   coordinateList.empty();
 
   sevenWonders.forEach(function(wonder) {
+    let URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${wonder}&key=AIzaSyArO73wSW8bJ-WDmqcrVXXNkAh0gFjeP14`
     axios.get(URL)
 
     .then ((response) => {
-      coordinateList.append(`<li>${response.data.}</li>`);
+      coordinateList.append(`<li>${wonder}<ul> <li>Latitude: ${response.data.results[0].geometry.location.lat}</li><li>Longitude: ${response.data.results[0].geometry.location.lng}</li><ul></li>`);
+      reportStatus('Wonders Loaded!');
     })
     .catch((error) => {
       console.log(error);
+      reportStatus(`Error: ${error.message}`)
     });
   });
 }
